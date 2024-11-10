64674272e67e039c02306de3493aa8c1e9ad8db8
@griffoo91 <omboggorised91@gmail.com>
2024-11-10 16:15:17 +0300

    2nd commit: login_screen user interface implementation and signup screen created.

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:airbnb_mobile/view/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,6 +10,11 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailTextEditingController = TextEditingController();
+  TextEditingController _passwordTextEditingController =
+      TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,6 +43,78 @@ class _LoginScreenState extends State<LoginScreen> {
                 color: Colors.white,
               ),
               textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 26),
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: "email"),
+                        style: const TextStyle(
+                          fontSize: 24,
+                        ),
+                        controller: _emailTextEditingController,
+                        validator: (valueEmail) {
+                          if (!valueEmail!.contains("@")) {
+                            return "Please enter email address.";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 21),
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: "password"),
+                        style: const TextStyle(
+                          fontSize: 24,
+                        ),
+                        controller: _passwordTextEditingController,
+                        validator: (valuePassword) {
+                          if (valuePassword!.length < 5) {
+                            return "Password must be 6 characters or more.";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.pink,
+                          padding: const EdgeInsets.symmetric(horizontal: 90),
+                        ),
+                        child: const Text(
+                          "LOGIN",
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Get.to(SignupScreen());
+                      },
+                      child: const Text(
+                        "Don't have an account? create here",
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
